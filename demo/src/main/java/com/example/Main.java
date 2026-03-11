7c2985b079d01088ec30a7a6d553dcecbf83b046
Samir-Hurtado <hurtadosamir20@gmail.com>
2026-03-10 21:01:57 -0500

    creando la calse main y llamando los objetos con sus metodos

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,36 @@
 package com.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     Scanner sc = new Scanner(System.in);
+
+     Libro libro1 = new Libro("las aventuras de samir", "Samir Hurtado", 5, 2);
+     
+     Libro libro2 = new Libro();
+
+     System.out.println("Ingrese el nombre del libro:");
+     libro2.setTitulo(sc.nextLine());
+
+     System.out.println("Ingrese el autor del libro:");
+     libro2.setAutor(sc.nextLine());
+
+     System.out.println("Ingrese el numero de ejemplares:");
+     libro2.setNumEjemplares(sc.nextInt());
+
+     libro2.setNumEjemplaresPrestados(0);
+
+     libroTextoUniajc libro3 = new libroTextoUniajc("Matematicas", "Alexandre", 10, 3, "Matematicas", "Ingenieria");
+
+     Novela novela = new Novela("El Gran Gatsby", "F. Scott Fitzgerald", 7, 1, "Novela Clásica");
+     
+     libro1.prestamo();
+     libro1.devolucion();
+
+
+     System.out.println(libro1);
+        System.out.println(libro2);
+        System.out.println(libro3);
+        System.out.println(novela);
     }
 }
