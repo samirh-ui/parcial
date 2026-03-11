070d5e6bfab9ed48caebf3b33a3506a536c213cc
Samir-Hurtado <hurtadosamir20@gmail.com>
2026-03-10 20:33:17 -0500

    agrendo metodos get y set

--- demo/src/main/java/com/example/libroTexto.java
+++ demo/src/main/java/com/example/libroTexto.java
@@ -3,6 +3,29 @@ package com.example;
 public class libroTexto  extends Libro{
     private String curso;
 
+
+    public libroTexto(String titulo, String autor, int numEjemplares, int numEjemplaresPrestados, String curso) {
+        super(titulo, autor, numEjemplares, numEjemplaresPrestados);
+        this.curso = curso;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    @Override   
+    public String toString() {
+        return super.toString() + "Curso: " + curso;
+    }
+
+    
+
+
+
     
     
 }
