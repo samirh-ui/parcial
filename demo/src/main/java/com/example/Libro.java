e9cbc11c6328b15e63fab86d02cf3904f1281f84
Samir-Hurtado <hurtadosamir20@gmail.com>
2026-03-10 19:42:55 -0500

    implementacion de la clase libro con sus atributos

---
+++ demo/src/main/java/com/example/Libro.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Libro {
+
+    public String titulo;
+    public String autor;
+    public int numEjemplares;
+    public int numEjemplaresPrestados;
+
+   
+
+
+    
+}
